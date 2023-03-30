86fe73abd32b7e06c644d659e1b005eca9fe55f7
Sushil <baymax732001@gmail.com>
2023-03-30 23:19:14 +0530

    learned about findviewbyid and setonclick listner

--- app/src/main/java/com/example/bmi_calculater/MainActivity.java
+++ app/src/main/java/com/example/bmi_calculater/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.bmi_calculater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText enterHI,emterW,enterHF;
+        Button btnCalculate;
+        TextView txtrst;
+        emterW = findViewById(R.id.edtWeight);
+        enterHF = findViewById(R.id.edtHeight);
+        enterHI = findViewById(R.id.edtHeightIn);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        txtrst = findViewById(R.id.result);
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
