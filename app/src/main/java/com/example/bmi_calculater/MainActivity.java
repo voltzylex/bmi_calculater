0bec9385290b89c548ed5076accff767319b47c8
Sushil <baymax732001@gmail.com>
2023-05-28 21:04:17 +0530

    bmi calculater finished

--- app/src/main/java/com/example/bmi_calculater/MainActivity.java
+++ app/src/main/java/com/example/bmi_calculater/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.bmi_calculater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText enterHI,emterW,enterHF;
+        EditText enterHI,enterW,enterHF;
         Button btnCalculate;
         TextView txtrst;
-        emterW = findViewById(R.id.edtWeight);
+        LinearLayout firstPage;
+        firstPage = findViewById(R.id.firstPage);
+        enterW = findViewById(R.id.edtWeight);
         enterHF = findViewById(R.id.edtHeight);
         enterHI = findViewById(R.id.edtHeightIn);
         btnCalculate = findViewById(R.id.btnCalculate);
@@ -25,6 +29,27 @@ public class MainActivity extends AppCompatActivity {
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+          int wt =  Integer.parseInt( enterW.getText().toString());
+                int hf =  Integer.parseInt( enterHF.getText().toString());
+                int hi =  Integer.parseInt( enterHI.getText().toString());
+                Log.d("ADebugTag", "Value: " +wt+hi+hf);
+                int totalInch = hf*12 + hi;
+                double totalC = totalInch*2.53;
+                double totalMe = totalC/100;
+                double bmi = wt/(totalMe*totalMe);
+                if(bmi>25){
+//                      android:backgroundTint="@color/teal_200"
+                        txtrst.setText("You are Overweight");
+                        firstPage.setBackgroundColor(getResources().getColor(R.color.overWeight));
+                }else if(bmi<18){
+                txtrst.setText("You are Healthy Dont Worry");
+                    firstPage.setBackgroundColor(getResources().getColor(R.color.normatlWeight));
+                }else{
+                    txtrst.setText("You are UnderWeight");
+                    firstPage.setBackgroundColor(getResources().getColor(R.color.underWeight));
+                }
+
+
 
             }
         });
